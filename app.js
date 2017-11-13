0a6203b6a3189aa786e9fcf2a36cce861827a9d3
Kalyan <justlikethatkalyan@gmail.com>
2017-11-13 17:20:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', express.static(path.join(__dirname, 'client/index.html')));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
