61aff6e39f6e2ee61cab0efb583e3739e26a1aec
Kalyan <justlikethatkalyan@gmail.com>
2017-11-13 17:26:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var cors = require('cors');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
 app.use(cors());
